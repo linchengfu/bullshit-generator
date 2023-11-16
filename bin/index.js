da2914c67611bbcd53168b4b98826d1061096377
linchengfu <linchengfu2000@gmail.com>
2023-11-16 18:06:18 +0800

    feat: process实现带参命令

--- bin/index.js
+++ bin/index.js
@@ -1,16 +1,19 @@
 import { loadCorpus, saveCorpus } from "../lib/corpus.js";
 import { generate } from "../lib/generator.js";
 import { createRandomPicker } from "../lib/random.js";
+import { options } from "../lib/cmd.js";
+
+
+
 
 
 const corpus = loadCorpus('corpus/data.json');
 
 const pickTitle = createRandomPicker(corpus.title);
 
-const title = pickTitle();
+const title = options.title || pickTitle();
 
-const article = generate(title, { corpus })
+const article = generate(title, { corpus, options })
 
-// console.log(`${title}\n\n    ${article.join('\n    ')}`)
 
 saveCorpus(title, article)
