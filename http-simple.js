4eadc158b1748f635964ae5741c9d8095d71ba52
linchengfu2000@gmail.com <linchengfu2000@gmail.com>
2024-02-22 02:45:00 +0800

    协商缓存

--- http-simple.js
+++ http-simple.js
@@ -1,11 +1,20 @@
-import http from 'http'
-import url from 'url'
-import path from 'path'
-import fs from 'fs'
-import mime from 'mime'
+// import http from 'http'
+// import url from 'url'
+// import path from 'path'
+// import fs from 'fs'
+// import mime from 'mime'
+
+const http = require('http');
+const url = require('url');
+const path = require('path');
+const fs = require('fs');
+const mime = require('mime')
 
 const server = http.createServer((req, res) => {
-  let filePath = path.resolve(__dirname, path.join('www', url.fileURLToPath(`file:///${req.url}`)));
+  // let filePath = path.resolve(__dirname, path.join('www', url.fileURLToPath(`file:///${req.url}`)));
+
+  let filePath = path.join(__dirname, 'www', req.url);
+  console.log('path', filePath)
 
   if (fs.existsSync(filePath)) {
     const stats = fs.statSync(filePath);
@@ -14,12 +23,28 @@ const server = http.createServer((req, res) => {
     }
     if (fs.existsSync(filePath)) {
       const { ext } = path.parse(filePath);
-      res.writeHead(200, {
+      const stats = fs.statSync(filePath);
+      const timeStamp = req.headers['if-modified-since'];
+
+      let status = 200;
+
+      if (timeStamp && Number(timeStamp) === stats.mtimeMs) {
+        // 如果timeStamp和stats.mtimeMS相等，说明文件内容没有修改
+        status = 304
+      }
+      res.writeHead(status, {
         'Content-Type': mime.getType(ext),
         'Cache-Control': 'max-age=86400', // 缓存一天
+        'last-modified': stats.mtimeMs,
       });
-      const fileStream = fs.createReadStream(filePath);
-      fileStream.pipe(res);
+
+      if (status === 200) {
+        const fileStream = fs.createReadStream(filePath)
+        fileStream.pipe(res)
+      } else {
+        res.end() // 如果状态码不是200，则不需要返回body
+      }
+
     }
   } else {
     res.writeHead(404, { 'Content-Type': 'text/html' });
@@ -27,4 +52,13 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen()
+
+
+
+server.on('clientError', (err, socket) => {
+  socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
+});
+
+server.listen(8080, () => {
+  console.log('opened server on', server.address());
+});
