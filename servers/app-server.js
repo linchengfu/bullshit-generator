27c3c321171704aa155471e203b8311778b36c5c
linchengfu2000@gmail.com <linchengfu2000@gmail.com>
2024-03-11 00:23:39 +0800

    handlebars

--- servers/app-server.js
+++ servers/app-server.js
@@ -5,6 +5,13 @@ import {
 } from "./interceptor-server.js";
 import Router from "./route.js";
 import { getCoronavirusKeyIndex, getCoronavirusByDate } from '../lib/module/mock.js'
+import handlebars from 'handlebars'
+import fs from 'fs'
+import url from 'url'
+import path from 'path'
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 const router = new Router();
 
@@ -29,19 +36,37 @@ App.use(param)
 // })
 
 App.use(router.get('/coronavirus/index', async ({ route, res }, next) => {
-  console.log('index')
-  const index = await getCoronavirusKeyIndex()
+
+  const index = getCoronavirusKeyIndex()
+  const filePath = path.resolve(__dirname, './templates/coronavirus_index.html')
+  const tpl = fs.readFileSync(filePath, { encoding: 'utf-8' })
+
+  // 编译模板
+  const template = handlebars.compile(tpl)
+
+  const result = template({ data: index })
+
+  let cleanedHtml = result.replace(/\r?\n|\r/g, "");
 
 
-  res.setHeader('Content-Type', 'application/json');
-  res.body = { data: index };
+  res.setHeader('Content-Type', 'text/html');
+  res.body = cleanedHtml;
   await next();
 }));
 
 App.use(router.get('/coronavirus/:date', async ({ route, res }, next) => {
   const data = getCoronavirusByDate(route.date);
-  res.setHeader('Content-Type', 'application/json');
-  res.body = { data };
+  const filePath = path.resolve(__dirname, './templates/coronavirus_data.html')
+
+  const tpl = fs.readFileSync(filePath, { encoding: 'utf-8' })
+
+  const template = handlebars.compile(tpl)
+
+  const result = template({ data })
+
+  res.setHeader('Content-Type', 'text/html');
+  res.body = result;
+
   await next();
 }));
 
